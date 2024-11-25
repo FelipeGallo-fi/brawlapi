6eefbcff2e7500faadac6b5ca9e2fa2157eb61a4
FelipeGallo05 <gallofelipe6@gmail.com>
2024-11-25 17:13:38 -0300

    Docker yaml

--- back/app.js
+++ back/app.js
@@ -1,11 +1,24 @@
 const express = require('express')
 const app = express()
 const port = 3000
+app.use(express.json())
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+let personajes = [
+  {
+    id: 1,
+    nombre: 'Goku',
+    poder: 9000
+  },
+  {
+    id: 2,
+    nombre: 'Vegeta',
+    poder: 8000
+  }
+]
+
+app.get('/api/v1/personajes', (req, res) => {
+  res.status(200).json(personajes);
 })
-
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
